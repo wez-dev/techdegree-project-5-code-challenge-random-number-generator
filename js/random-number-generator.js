1a3b4c258114966e4abae12f0cd69b4e293949fe
wez-dev <76491516+wez-dev@users.noreply.github.com>
2021-03-14 17:41:09 +0000

    Data type validation
    Check user has entered numbers. If NaN, return error message

--- js/random-number-generator.js
+++ js/random-number-generator.js
@@ -1,7 +1,7 @@
-// 0. Access the HTML
+// 1. Access the HTML
 const main = document.querySelector('main');
 
-// 1. Collect input from a user
+// 2. Collect input from a user
 let lowerLimit = +prompt(`
 Which number should we start generating random numbers from?
 `);
@@ -10,16 +10,31 @@ const upperLimit = +prompt(`
 What's the maximum random number we should generate?
 `);
 
-// 2. Convert the input to a number 
-// Note: Used the uniary plus operator to convert input to numbers on input capture (see step above)
-
-// 3. Use Math.random() and the user's number to generate a random number
-// multiply random [range between 0 - <1], then increment by the lowerlimit to ensure no result is below the lower limit 
-const randomNumber = Math.floor(Math.random() * upperLimit) + lowerLimit;
-
-// 4. Create a message displaying the random number
-main.innerHTML = `
-<h1>Random Number Roller</h1>
-<p> You rolled a random number between ${lowerLimit} and ${upperLimit} ... </p>
-<p> ... and it's a <strong>${randomNumber}</strong>! </P
-`;
+// 3. Check input -- if NaN return error message.
+if (isNaN(lowerLimit) ) {
+    main.innerHTML = `
+    <h1>Random Number Roller</h1>
+    <p> Ouch!</p>
+    <p>We only roll numbers - refresh the page to try again!</p>
+    `;
+}else if (isNaN(upperLimit)) {
+    main.innerHTML = `
+    <h1>Random Number Roller</h1>
+    <p> Ouch!</p>
+    <p>We only roll numbers - refresh the page to try again!</p>
+    `;
+}else {
+    // 4. Convert the input to a number 
+    // Note: Used the uniary plus operator to convert input to numbers on input capture (see step 2)
+    
+      // 5. Use Math.random() and the user's number to generate a random number
+    // multiply random [range between 0 - <1], then increment by the lowerlimit to ensure no result is below the lower limit 
+    const randomNumber = Math.floor(Math.random() * upperLimit) + lowerLimit;
+    
+    // 4. Create a message displaying the random number
+    main.innerHTML = `
+    <h1>Random Number Roller</h1>
+    <p> You rolled a random number between ${lowerLimit} and ${upperLimit} ... </p>
+    <p> ... and it's a <strong>${randomNumber}</strong>! </p>
+    `;
+}
