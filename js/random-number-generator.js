00e5903bd5b3097dd0da340cad3b35d8282d1ac6
wez-dev <76491516+wez-dev@users.noreply.github.com>
2021-04-20 21:22:01 +0100

    Re-structured as function with JSDocs
    * Random number generator re-structured as function - no code executes outside function except initial function call
    * Two functions: [1] Collects user input and passes params to function 2 [2] Validates params and generates random number

--- js/random-number-generator.js
+++ js/random-number-generator.js
@@ -1,52 +1,70 @@
-/**
- * @const {string} main - Access document html
- */
-const main = document.querySelector('main');
 
 /**
- * @const {string} lowerLimit - User define lower range limit for random number generator
+ * Collect user input and passes params to randomNumberGen()
+ * 
+ * @const {number} lowerLimit     - user defined lower range limit for random number
+ * @const {number} upperLimit     - user defined upper range limit for random number 
  */
-const lowerLimit = +prompt(`
-Which number should we start generating random numbers from?
-`);
-
-/**
- * @const {string} upperLimit - User define upper range limit for random number generator
- */
-const upperLimit = +prompt(`
-What's the maximum random number we should generate?
-`);
+function getRandomNumberLimits(){
 
 
-// Validate user input -- if either or are NaN return error message.
+    // Note: Used the unary plus operator to convert "3" to 3 (string to number) on input capture 
+    const lowerLimit = +prompt(`
+    Which number should we start generating random numbers from?
+    `);
 
-if (isNaN(lowerLimit) || isNaN(upperLimit) ) {
-    main.innerHTML = `
-    <h1>Random Number Roller</h1>
-    <p> Ouch!</p>
-    <p>We only roll numbers - refresh the page to try again.</p>
-    `;
-}else if (upperLimit < lowerLimit) {
-    main.innerHTML = `
-    <h1>Random Number Roller</h1>
-    <p> Ouch!</p>
-    <p>The lower limit cannot be higher than the upper limit - refresh the page to and try again.</p>
-    `;
-} else {
-    // Use Math.random() and the user's lower and higher limitts to generate a random number
-    // Note: Used the unary plus operator to convert input to numbers on input capture 
+    // Note: Used the unary plus operator to convert "3" to 3 (string to number) on input capture 
+    const upperLimit = +prompt(`
+    What's the maximum random number we should generate?
+    `);
 
-    /**
-     * highNumber - lowNumber + 1, which represents the range of numbers you want. 
-     * For example, if you wanted a number between 10 and 15, (15 - 10 + 1) gives you 6,
-     * which is how many numbers to choose from (10, 11, 12, 13, 14 and 15).
-    * @const {string} randomNumber - The random number to return
-     */
-    const randomNumber = Math.floor(Math.random() * (upperLimit - lowerLimit + 1) ) + lowerLimit;
-    // Create a message displaying the random number
-    main.innerHTML = `
-    <h1>Random Number Roller</h1>
-    <p> You rolled a random number between ${lowerLimit} and ${upperLimit} ... </p>
-    <p> ... and it's a <strong>${randomNumber}</strong>! </p>
-    `;
+    // Pass user defined values to the random number function
+    randomNumberGen(lowerLimit, upperLimit);
 }
+
+
+/**
+ * Generate a random number between a range set by params and return results inside <main> tag
+ * 
+ * @param {number} lower     - user defined lower range limit for random number
+ * @param {number} upper     - user defined upper range limit for random number 
+ * @const {string} main           - Access document HTML
+ * @const {number} randomNumber - Random number within user defined limits
+ */
+function randomNumberGen(lower, upper){
+    
+    const main = document.querySelector('main');
+
+    // Validate user input -- if either or are NaN return error message.
+    if (isNaN(lower) || isNaN(upper) ) {
+        main.innerHTML = `
+        <h1>Random Number Roller</h1>
+        <p> Ouch!</p>
+        <p>We only roll numbers - refresh the page to try again.</p>
+        `;
+    }else if (upper < lower) {
+        main.innerHTML = `
+        <h1>Random Number Roller</h1>
+        <p> Ouch!</p>
+        <p>The lower limit cannot be higher than the upper limit - refresh the page to and try again.</p>
+        `;
+    } else {
+        /* Use Math.random() and the user's lower and higher limitts to generate a random number
+        * highNumber - lowNumber + 1, which represents the range of numbers you want. 
+        * For example, if you wanted a number between 10 and 15, (15 - 10 + 1) gives you 6,
+        * which is how many numbers to choose from (10, 11, 12, 13, 14 and 15).
+        * Note: Used the unary plus operator to convert "3" to 3 (string to number) on input capture  
+        */
+        const randomNumber = Math.floor(Math.random() * (upper - lower + 1) ) + lower;       
+        main.innerHTML = `
+        <h1>Random Number Roller</h1>
+        <p> You rolled a random number between ${lower} and ${upper} ... </p>
+        <p> ... and it's a <strong>${randomNumber}</strong>! </p>
+        `;
+    }
+}
+
+
+getRandomNumberLimits();
+
+
